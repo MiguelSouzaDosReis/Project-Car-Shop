d34288fd5fc615763ff678543a671e8a53f0b933
Miguel_Reis <guelsouzar@gmail.com>
2022-06-18 20:14:05 -0300

    model && service 100%

--- src/services/CarServices.ts
+++ src/services/CarServices.ts
@@ -9,20 +9,9 @@ class CarService extends Service<Car> {
 
   create = async (obj: Car): Promise<Car | ServiceError | null> => {
     const parsed = CarSchema.safeParse(obj);
-    if (!parsed.success) {
-      return { error: parsed.error };
-    }
+    if (!parsed.success) { return { error: parsed.error }; }
     return this.model.create(obj);
   };
-
-  async update(
-    id: string,
-    obj: Car,
-  ): Promise<Car | null | ServiceError> {
-    const parsed = CarSchema.safeParse(obj);
-    if (!parsed.success) return { error: parsed.error };
-    return this.model.update(id, obj);
-  }
 }
 
 export default CarService;
--- src/tests/unit/models/model.test.ts
+++ src/tests/unit/models/model.test.ts
@@ -26,23 +26,23 @@ describe('Model', () => {
     expect(model.readOne).to.be.an('function');
     expect(model.update).to.be.an('function');
   });
+  const carExemple = {
+    _id: '628fafea3b94a2894ec3f9e7',
+    model: 'aaa',
+    year: 1902,
+    color: 'bbb',
+    status: true,
+    buyValue: 2,
+    doorsQty: 2,
+    seatsQty: 2
+  }
   describe('CRUD functions working', () => {
-    const id = '123456789';
-    const carExemple = {
-      _id: '1',
-      model: 'a',
-      year: 1902,
-      color: 'a',
-      buyValue: 1,
-      doorsQty: 1,
-      seatsQty: 1,
-    }
     it('create', async () => {
       await model.create(carExemple);
       expect(functions.create.called).to.be.true;
     });
     it('delete', async () => {
-      await model.delete(id);
+      await model.delete(carExemple._id);
       expect(functions.findOneAndDelete.called).to.be.true;
     });
     it('read', async () => {
@@ -50,43 +50,34 @@ describe('Model', () => {
       expect(functions.find.called).to.be.true;
     });
     it('readOne', async () => {
-      await model.readOne(id);
+      await model.readOne(carExemple._id);
       expect(functions.findOne.called).to.be.true;
     });
     it('update', async () => {
-      await model.update(id, carExemple);
+      await model.update(carExemple._id, carExemple);
       expect(functions.findOneAndUpdate.called).to.be.true;
     });
   });
-});
-describe("CarModel", () => {
-  let carModel =  new CarModel();
-  const carExemple = {
-    _id: '1',
-    model: 'a',
-    year: 1902,
-    color: 'a',
-    buyValue: 1,
-    doorsQty: 1,
-    seatsQty: 1,
-  }
-  describe(('create'), () => {
-    before(() => {
-      Sinon.stub(carModel, 'create').resolves(carExemple)
-    })
-    after(() => {
-      (carModel.create as Sinon.SinonStub).restore()
-    })
-    it(('retorna um objeto'), async() => {
-      const carCreate = await carModel.create(carExemple);
-
-      expect(carCreate).to.be.an('object');
-      expect(carCreate).to.have.property('model');
-      expect(carCreate).to.have.property('year');
-      expect(carCreate).to.have.property('color');
-      expect(carCreate).to.have.property('buyValue');
-      expect(carCreate).to.have.property('seatsQty');
-      expect(carCreate).to.have.property('doorsQty');
+  describe("CarModel", () => {
+    let carModel =  new CarModel();
+    describe(('create'), () => {
+      before(() => {
+        Sinon.stub(carModel, 'create').resolves(carExemple)
+      })
+      after(() => {
+        (carModel.create as Sinon.SinonStub).restore()
+      })
+      it(('retorna um objeto'), async() => {
+        const carCreate = await carModel.create(carExemple);
+  
+        expect(carCreate).to.be.an('object');
+        expect(carCreate).to.have.property('model');
+        expect(carCreate).to.have.property('year');
+        expect(carCreate).to.have.property('color');
+        expect(carCreate).to.have.property('buyValue');
+        expect(carCreate).to.have.property('seatsQty');
+        expect(carCreate).to.have.property('doorsQty');
+      })
     })
   })
-})
+});
